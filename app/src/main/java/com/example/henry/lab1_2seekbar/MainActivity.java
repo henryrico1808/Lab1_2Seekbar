50aa47b4d81b43d7eed1f32751ffee7ec8849c21
henryrico1808 <36237658+henryrico1808@users.noreply.github.com>
2018-02-12 11:37:25 -0700

    cambio para listview

--- app/src/main/java/com/example/henry/lab1_2seekbar/MainActivity.java
+++ app/src/main/java/com/example/henry/lab1_2seekbar/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.henry.lab1_2seekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
     TextView textView, textView2;
 
+    ListView lista;
+    ArrayAdapter<String> adapter;
+    String valores[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +25,24 @@ public class MainActivity extends AppCompatActivity {
         seekBar = findViewById(R.id.seekBar);
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
+        lista = findViewById(R.id.listview);
 
+        valores = new String[30];
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,valores);
 
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textView2.setText("");
-                cad="";
+                //cad="";
                 textView.setText(progress+"");
                 for (int i=0;i<seekBar.getMax();i++){
-                    cad+= progress+"x"+(i+1)+" ="+progress*(i+1)+"\n";
+                    //cad+= progress+"x"+(i+1)+" ="+progress*(i+1)+"\n";
+                    valores[i] = progress+"x"+(i+1)+" ="+progress*(i+1);
                 }
-
-                textView2.setText(cad);
+                lista.setAdapter(adapter);
+                //textView2.setText(cad);
 
 
 
